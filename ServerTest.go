fd55d28c298cef1799b055be040eb0dc561e8cae
budnyaha <67742202+budnyaha@users.noreply.github.com>
2020-09-11 15:03:10 +0200

    asd

--- ServerTest.go
+++ ServerTest.go
@@ -13,11 +13,41 @@ import (
 	"net/http"
 	"time"
 
-	//"MyPackges/crypt"
-
 	"github.com/maxchagin/go-memorycache-example"
 )
 
+// Структура xml файла
+type Courses struct {
+	Data []struct {
+		Rows []struct {
+			Row []struct {
+				Boardid                 string  `xml:"BOARDID,attr"`
+				TradeDate               string  `xml:"TRADEDATE,attr"`
+				ShortName               string  `xml:"SHORTNAME,attr"`
+				SecId                   string  `xml:"SECID,attr"`
+				NumTrades               int64   `xml:"NUMTRADES,attr"`
+				Value                   float64 `xml:"VALUE,attr"`
+				Open                    float64 `xml:"OPEN,attr"`
+				Low                     float64 `xml:"LOW,attr"`
+				High                    float64 `xml:"HIGH,attr"`
+				LegalClosePrice         float64 `xml:"LEGALCLOSEPRICE,attr"`
+				WapPrise                float64 `xml:"WAPRICE,attr"`
+				Close                   float64 `xml:"CLOSE,attr"`
+				Volume                  int64   `xml:"VOLUME,attr"`
+				MarketPrice2            float64 `xml:"MARKETPRICE2,attr"`
+				MarketPrice3            float64 `xml:"MARKETPRICE3,attr"`
+				AdmittedQuote           float64 `xml:"ADMITTEDQUOTE,attr"`
+				Mp2Valtrd               float64 `xml:"MP2VALTRD,attr"`
+				MarketPrice3TradesValue float64 `xml:"MARKETPRICE3TRADESVALUE,attr"`
+				AdmittedValue           float64 `xml:"ADMITTEDVALUE,attr"`
+				Waval                   int64   `xml:"WAVAL,attr"`
+				TradingSession          int64   `xml:"TRADINGSESSION,attr"`
+			} `xml:"row" json:"Row"`
+		} `xml:"rows"`
+	} `xml:"data"`
+}
+
+// Метод дешифровки
 func Decrypt(CriptResult interface{}, Key []byte, Nonce []byte) []byte {
 
 	CriptText, _ := CriptResult.([]byte)
@@ -40,6 +70,7 @@ func Decrypt(CriptResult interface{}, Key []byte, Nonce []byte) []byte {
 	return Result
 }
 
+// Метод шифровки
 func Encrypt(Result []byte) ([]byte, []byte, []byte) {
 	Key, _ := hex.DecodeString("6368616e676520746869732070617373776f726420746f206120736563726574")
 
@@ -64,6 +95,7 @@ func Encrypt(Result []byte) ([]byte, []byte, []byte) {
 	return Ciphertext, Key, Nonce
 }
 
+// Метод получения данных с сайта
 func getXML(url string) ([]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -83,6 +115,7 @@ func getXML(url string) ([]byte, error) {
 	return data, nil
 }
 
+// Метод обработки данных в Json
 func Content() []byte {
 	xmlFile, err := getXML("https://iss.moex.com/iss/history/engines/stock/markets/shares/boards/tqbr/securities.xml")
 	if err != nil {
@@ -112,6 +145,8 @@ func main() {
 	var Key, Nonce []byte
 
 	http.HandleFunc("/courses", func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Add("Content-Type", "application/json")
 
 		CriptResult, err := Cache.Get("myKey")
 
